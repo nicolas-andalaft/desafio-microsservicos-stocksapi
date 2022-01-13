6faacb623c25036178fc2c37c4f2baa799010f1b
Nicolas Andalaft <nicolas.andalaft@hotmail.com>
2022-01-13 11:20:18 -0300

    API importada

---
+++ src/main/java/com/nicolas/stocksapi/StocksapiApplication.java
@@ -0,0 +1,13 @@
+package com.nicolas.stocksapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StocksapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StocksapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nicolas/stocksapi/StocksapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nicolas.stocksapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StocksapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
