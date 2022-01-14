bad4df9b7b8d2ff366d285ded38baecdd0aab666
Nicolas Andalaft <nicolas.andalaft@hotmail.com>
2022-01-14 16:04:01 -0300

    Removidos códigos utilizados para testes

--- src/main/java/com/nicolas/stocksapi/StocksapiApplication.java
+++ src/main/java/com/nicolas/stocksapi/StocksapiApplication.java
@@ -2,10 +2,6 @@ package com.nicolas.stocksapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.oauth2.core.oidc.user.OidcUser;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class StocksapiApplication {
@@ -13,18 +9,4 @@ public class StocksapiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(StocksapiApplication.class, args);
 	}
-
-	@RestController
-    static class SimpleRestController {
-        @GetMapping("/")
-        String sayHello(@AuthenticationPrincipal OidcUser oidcUser) {
-            return "Hello: " + oidcUser.getFullName();
-        }
-
-        @GetMapping("/teste")
-        String teste(@AuthenticationPrincipal OidcUser oidcUser) {
-            return "Hello: " + oidcUser.getBirthdate();
-        }
-    }
-
 }
