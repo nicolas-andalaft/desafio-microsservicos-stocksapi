91306da81981bcca1f3fdb1d5776bef1083659cf
Nicolas Andalaft <nicolas.andalaft@hotmail.com>
2022-01-14 17:57:57 -0300

    Configuração do CORS

--- src/main/java/com/nicolas/stocksapi/StocksapiApplication.java
+++ src/main/java/com/nicolas/stocksapi/StocksapiApplication.java
@@ -2,6 +2,10 @@ package com.nicolas.stocksapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @SpringBootApplication
 public class StocksapiApplication {
@@ -9,4 +13,16 @@ public class StocksapiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(StocksapiApplication.class, args);
 	}
+
+	@Configuration
+    @EnableWebSecurity
+    class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http.cors().and()
+                    .authorizeRequests(authorizeRequests -> authorizeRequests.anyRequest().authenticated())
+                    .oauth2ResourceServer().jwt();
+        }
+    }
 }
